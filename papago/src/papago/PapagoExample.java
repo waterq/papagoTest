ff530f2c18860b88278544fddfa1ff135f7e23d7
이동훈 <@DESKTOP-3NPMHCD.lgcns.com>
2021-10-12 18:06:51 +0900

    번역 소스 정리

--- papago/src/papago/PapagoExample.java
+++ papago/src/papago/PapagoExample.java
@@ -6,17 +6,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import util.PapagoTran;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PapagoExample {
 
 	private String[] langs = { "en", "ja", "zh-CN", "ru" };
 
-	public static void main(String[] args) throws MalformedURLException, IOException, ParseException {
+	public static void main(String[] args) throws MalformedURLException, IOException {
 		PapagoExample example = new PapagoExample();
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("천인계획");
@@ -26,26 +23,26 @@ public class PapagoExample {
 
 		for (String text : list) {
 			Map<String, String> result = example.translate(text);
-			System.out.println(result);
+			System.out.println(text + " : " + result);
 		}
 	}
 
-	private Map<String, String> translate(String text) throws MalformedURLException, IOException, ParseException {
+	@SuppressWarnings("unchecked")
+	private Map<String, String> translate(String text) throws MalformedURLException, IOException {
+		Map<String, String> transMap = new HashMap<String, String>();
 		PapagoTran papagoTran = PapagoTran.getPapagoTran();
-		JSONParser parser = new JSONParser();
-
-		Map<String, String> result = new HashMap<String, String>();
+		ObjectMapper mapper = new ObjectMapper();
 		for (String lang : langs) {
 			String translate = papagoTran.translate(text, lang);
-			
-			JSONObject json = (JSONObject) parser.parse(translate);
-			JSONObject json1 = (JSONObject)json.get("message");
-			JSONObject json2 = (JSONObject)json1.get("result");
-			
-			result.put(lang, json2.get("translatedText").toString());
+			Map<String, Object> json = mapper.readValue(translate, new TypeReference<Map<String, Object>>() {
+			});
+			Map<String, Object> message = (Map<String, Object>) json.get("message");
+			Map<String, Object> result = (Map<String, Object>) message.get("result");
+
+			transMap.put(lang, result.get("translatedText").toString());
 		}
 
-		return result;
+		return transMap;
 	}
 
 }
--- papago/src/papago/PapagoTran.java
+++ papago/src/papago/PapagoTran.java
@@ -1,4 +1,4 @@
-package util;
+package papago;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
